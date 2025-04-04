92ad5d4c51cad848edfd0612c8c89c671ef15980
sancar17 <sancar17@itu.edu.tr>
2025-04-04 15:20:32 +0200

    configs done

--- configs.py
+++ configs.py
@@ -7,30 +7,30 @@
 ROOM_CONFIGS = {
     "room1": {
         "min_area": 1000,
-        "max_distance" : 10,
+        "max_distance" : 100,
         "epsilon": 0.015,
         "extend_length": 50,
-        "min_intersection_dist":200,
+        "min_intersection_dist":10,
         "min_line_length":60,
         "max_line_gap":150
 
     },
     "room2": {
         "min_area": 200,
-        "max_distance" : 2,
+        "max_distance" : 100,
         "epsilon": 0.015,
-        "extend_length": 25,
+        "extend_length": 50,
         "min_intersection_dist":20,
         "min_line_length":10,
-        "max_line_gap":100
+        "max_line_gap":40
     },
     "room3": {
-        "min_area": 200,
-        "max_distance" : 2,
+        "min_area": 20,
+        "max_distance" : 100,
         "epsilon": 0.015,
-        "extend_length": 25,
+        "extend_length": 50,
         "min_intersection_dist":20,
         "min_line_length":10,
-        "max_line_gap":100
+        "max_line_gap":40
     },
 }
